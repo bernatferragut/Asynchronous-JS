aa9638887d0c3cb3bec010662fb0c100bc5951e8
elbernat <elbernat@gmail.com>
2018-02-11 17:28:03 -0500

    Shuffle algorithm working
    Small changes to obtain a true shuffle algorithm finally implemented.

--- JS/main.js
+++ JS/main.js
@@ -3,13 +3,6 @@
 // Selections
 let selectedPads = document.getElementsByClassName('pad');
 
-// Action
-let isClicked = false;
-for(let i=0; i<selectedPads.length; i++) {
-    selectedPads[i].addEventListener('click', activated, isClicked);
-    selectedPads[i].innerHTML = randomAnswers();
-}
-
 // Logic
 
 function activated(event) {
@@ -25,29 +18,31 @@ function activated(event) {
 
 }
 
-// Creating random numbers for the cells
-function randomAnswers() {
-    return Math.floor(Math.random()*9)+1; // creates a random number between 1-9 
-}
-
 // Creating a Shuffle list 
 let myPairs = [1,1,2,2,3,3,4,4,5]; // potential numbers
-console.log('original list:' + myPairs);
+console.log('original: ' + myPairs);
 
 function myShuffle(myPairs) {
      // shuffle algorithm
      for( let i = myPairs.length-1; i>=0; i-- ) {
         // random number (0-9) of the list
-        let randMyPairs =  Math.floor(Math.random() * i+1);
+        let randMyPairs =  Math.floor(Math.random() * (i+1));
         let itemAtIndex = myPairs[randMyPairs];
         // values permutations
-        myPairs[itemAtIndex] = myPairs[i];
+        myPairs[randMyPairs] = myPairs[i];
         myPairs[i] = itemAtIndex;
      }
-     console.log('shuffled list:' + myPairs);
+     console.log('shuffled: ' + myPairs);
      return myPairs;
 }
 
 ;
-myShuffle(myPairs);
+let shuffledPairs = myShuffle(myPairs);
+
+// Action
+let isClicked = false;
+for(let i=0; i<selectedPads.length; i++) {
+    selectedPads[i].addEventListener('click', activated, isClicked);
+    selectedPads[i].innerHTML = shuffledPairs[i];
+}
 
