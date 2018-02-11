c61ac18bca30a675673a1eaa1e1b0b929efc1c94
elbernat <elbernat@gmail.com>
2018-02-10 19:24:21 -0500

    Sguffling Algorithm
    Shuffle algorithm function

--- JS/main.js
+++ JS/main.js
@@ -30,5 +30,24 @@ function randomAnswers() {
     return Math.floor(Math.random()*9)+1; // creates a random number between 1-9 
 }
 
-console.log(randomAnswers());
+// Creating a Shuffle list 
+let myPairs = [1,1,2,2,3,3,4,4,5]; // potential numbers
+console.log('original list:' + myPairs);
+
+function myShuffle(myPairs) {
+     // shuffle algorithm
+     for( let i = myPairs.length-1; i>=0; i-- ) {
+        // random number (0-9) of the list
+        let randMyPairs =  Math.floor(Math.random() * i+1);
+        let itemAtIndex = myPairs[randMyPairs];
+        // values permutations
+        myPairs[itemAtIndex] = myPairs[i];
+        myPairs[i] = itemAtIndex;
+     }
+     console.log('shuffled list:' + myPairs);
+     return myPairs;
+}
+
+;
+myShuffle(myPairs);
 
