6ad5eea785c82be623fa254a2396fd1f8b109b21
elbernat <elbernat@gmail.com>
2018-02-13 09:22:45 -0500

    Async game follow up code
    Async game follow up code

--- JS/main.js
+++ JS/main.js
@@ -27,7 +27,13 @@ let shuffledPairs = myShuffle(myPairs);
 //------------------------------------------------------------
 
 
-// LOGIC
+// Global variables
+let clickedArray = [];
+let interval;
+let started = false;
+let time = 0;
+let ready = true;
+let  numCompleted = 0;
 
 // functions executions
 setUp();
@@ -54,19 +60,72 @@ function setUp() {
         });
         // cell click
         cell.addEventListener('click', function(){
-            // cell active
-            if (cell.clicked == false) {
-                cell.clicked = true;
-                this.style.background = 'lightgreen';
-                console.log('now--->clicked');
-            } else {
-                cell.clicked = false;
-                console.log('now--->never clicked');
+            // when ready is false no click event handlers can be made
+            if(ready == false){
+                return;
             }
-            
-            
+            // start timer
+            startTimer();
+            // start
+            if(this.clicked == false && this.paired == false){
+                clickedArray.push(this);
+                showContent(this);
+            }
+            // matching pairs
+            if(clickedArray.length == 2) {
+                // a matching pair is found and passed as complete
+                complete(clickedArray[0]);
+                complete(clickedArray[1]);
+                // clicked array cleaned
+                clickedArray = [];
+                // when finished
+                if(numCompleted == 8) {
+                    alert('Youn won in ' + time + 'seconds');
+                    clearInterval(interval);
+                }
+            } else {
+                // if matching pair is not found
+                ready = false;
+                setTimeout(function() {
+                    // after 500 ms
+                },500);
+                hide(clickedArray[0]);
+                hide(clickedArray[1]);
+
+                clickedArray = [];
+                ready = HTMLPictureElement;
+
+            }
         });
     }
+}
+
+function showContent(cell) {
+    cell.style.background = 'red';
+    cell.innerHTML = cell.value;
+    cell.clicked = true;
+}
+
+function startTimer() {
+    if(started == false) {
+        interval = setInterval(function() {
+            time ++;
+            document.getElementById('timer').innerHTML = 't: ' + time;
+        }, 1000)
+    }
+    started = true;
+}
+
+function hide(cell) {
+    cell.style.background = '#cacbbb';
+    cell.innerHTML = '';
+    cell.clicked = false;
+}
+
+function complete(cell) {
+    numCompleted++;
+    cell.paired = true;
+    cell.style.background = 'green';
 }
 
 
