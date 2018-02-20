e485a45105ae7208a80d019a6e033db30d04a8c1
elbernat <elbernat@gmail.com>
2018-02-20 08:15:34 -0500

    Promises - basics
    Prmises - First Promise

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,25 @@
 // JS - Promises
 
-// selections
+// 1.Creating a Promise
+let prmoise1 = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        var val = Math.random();
+        if( val > .5){
+            resolve(val);
+        }
+        else {
+            reject(val);
+        }
+    }, 5000);
+});
 
-// Global variables
+// 2.Extracting the Promise Results with event and catch methods
 
-// function Initialisation
-setUp();
+prmoise1.then(function(val) {
+    console.log('Resolved: ' + val);
+}).catch(function(err){
+    console.log('Error: '+ err);
+});
 
-// function definitions
-function setUp() {
-    // code
-}
+// First Promise Pattern resolved with success !!!
+
